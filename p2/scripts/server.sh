23fbeaa7f3ae7a879b01402e7df6cef884403c23
Theo ZERIBI <tzeribi@gmail.com>
2024-03-25 15:51:49 +0100

    feat(p2): Start fixing p2

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Path: p1/scripts/server.sh										#
+# Path: p2/scripts/server.sh										#
 # Author: Theo ZERIBI												#
 # Date: 2024-03-14													#
 # Description: This script is used for setup the Server Machine.	#
